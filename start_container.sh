c45746d287196c155b0c924b5b5fd993f0783395
Raj Behere <117808263+RajBehere0190@users.noreply.github.com>
2024-07-19 17:49:09 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull rajbehere0190/simple-python-flask-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000
+docker run -d -p 5000:5000 rajbehere0190/simple-python-flask-app
