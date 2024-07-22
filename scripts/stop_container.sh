31669703eb03adba7bcb8260f290962361410d66
Raj Behere <117808263+RajBehere0190@users.noreply.github.com>
2024-07-22 21:02:45 +0530

    Update stop_container.sh

--- scripts/stop_container.sh
+++ scripts/stop_container.sh
@@ -2,14 +2,14 @@
 set -e
 
 # Define the name or ID of the container to stop
-CONTAINER_NAME="e2623fd3b03f"
+CONTAINER_NAME="cf2a8b0fcb4a"  # Replace with your actual container name or ID
 
 # Check if the container is running
-if [ "$(docker ps -q -f name=${heuristic_feistel})" ]; then
-    echo "Stopping container: ${heuristic_feistel}"
+if [ "$(docker ps -q -f name=${CONTAINER_NAME})" ]; then
+    echo "Stopping container: ${CONTAINER_NAME}"
     # Stop the container
-    docker stop ${heuristic_feistel}
-    echo "Container ${heuristic_feistel} stopped successfully."
+    docker stop ${CONTAINER_NAME}
+    echo "Container ${CONTAINER_NAME} stopped successfully."
 else
-    echo "No running container found with the name or ID: ${heuristic_feistel}"
+    echo "No running container found with the name or ID: ${CONTAINER_NAME}"
 fi
