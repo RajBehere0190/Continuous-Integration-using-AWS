032013bb54a37ecde6cb2889954f7347290e7246
Raj Behere <117808263+RajBehere0190@users.noreply.github.com>
2024-07-28 18:59:44 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -2,9 +2,7 @@
 set -e
 
 # Pull the Docker image from Docker Hub
-docker pull rajbehere0190/sample-python-app
+echo
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 rajbehere0190/sample-python-app
-
-
+echo
