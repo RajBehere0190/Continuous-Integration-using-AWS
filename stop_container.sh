885b93a82bf3eac12021705e8e1ca24e4b938fda
Raj Behere <117808263+RajBehere0190@users.noreply.github.com>
2024-07-28 18:21:03 +0530

    Update stop_container.sh

--- stop_container.sh
+++ stop_container.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 set -e
 
-# Define the name or ID of the container to stop
-CONTAINER_NAME="cf2a8b0fcb4a"  # Replace with your actual container name or ID
-
-# Check if the container is running
-if [ "$(docker ps -q -f name=${CONTAINER_NAME})" ]; then
-    echo "Stopping container: ${CONTAINER_NAME}"
-    # Stop the container
-    docker stop ${CONTAINER_NAME}
-    echo "Container ${CONTAINER_NAME} stopped successfully."
-else
-    echo "No running container found with the name or ID: ${CONTAINER_NAME}"
-fi
+# Stop the running container (if any)
+echo "Hi"
